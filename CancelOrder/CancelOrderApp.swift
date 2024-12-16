7805c53530109791add72dca2f2bbc0da0326a93
SanekWhite <bro.b98@mail.ru>
2024-12-16 21:24:55 +0700

    Initial Commit

---
+++ CancelOrder/CancelOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CancelOrderApp.swift
+//  CancelOrder
+//
+//  Created by Александр Белый on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CancelOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
